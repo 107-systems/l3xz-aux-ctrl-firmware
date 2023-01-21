ae1cf7a64bf01ff18dc218c67e9edba1cba6ca41
Alexander Entinger <consulting@lxrobotics.com>
2023-01-21 13:21:01 +0100

    Inial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+<a href="https://107-systems.org/"><img align="right" src="https://raw.githubusercontent.com/107-systems/.github/main/logo/107-systems.png" width="15%"></a>
+:floppy_disk: `l3xz-aux-ctrl-firmware`
+======================================
+<a href="https://opencyphal.org/"><img align="right" src="https://raw.githubusercontent.com/107-systems/.github/main/logo/opencyphal.svg" width="25%"></a>
+[![General Formatting Checks](https://github.com/107-systems/l3xz-aux-ctrl-firmware/workflows/General%20Formatting%20Checks/badge.svg)](https://github.com/107-systems/l3xz-aux-ctrl-firmware/actions?workflow=General+Formatting+Checks)
+[![Spell Check](https://github.com/107-systems/l3xz-aux-ctrl-firmware/workflows/Spell%20Check/badge.svg)](https://github.com/107-systems/l3xz-aux-ctrl-firmware/actions?workflow=Spell+Check)
+[![Compile Examples](https://github.com/107-systems/l3xz-aux-ctrl-firmware/workflows/Compile/badge.svg)](https://github.com/107-systems/l3xz-aux-ctrl-firmware/actions?workflow=Compile)
+
+Firmware for the [L3X-Z](https://github.com/107-systems/l3xz) auxiliary controller which is based on the [OpenCyphalPicoBase](https://github.com/generationmake/OpenCyphalPicoBase) board.
+
+<p align="center">
+  <a href="https://github.com/107-systems/l3xz"><img src="https://raw.githubusercontent.com/107-systems/.github/main/logo/l3xz-logo-memento-mori-github.png" width="40%"></a>
+</p>
+
+## How-to-build/upload
+```bash
+arduino-cli compile -b rp2040:rp2040:rpipico -v .
+arduino-cli upload -b rp2040:rp2040:rpipico -v . -p /dev/ttyACM0
+```
+
---
+++ l3xz-aux-ctrl-firmware.ino
@@ -0,0 +1,224 @@
+/**
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2023 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/l3xz-aux-ctrl-firmware/graphs/contributors.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <pico/stdlib.h>
+#include <hardware/watchdog.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Servo.h>
+
+#include <I2C_eeprom.h>
+#include <107-Arduino-Cyphal.h>
+#include <107-Arduino-MCP2515.h>
+#include <107-Arduino-UniqueId.h>
+#include <107-Arduino-CriticalSection.h>
+
+#undef max
+#undef min
+#include <algorithm>
+
+#include <Adafruit_NeoPixel.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+using namespace uavcan::node;
+using namespace uavcan::_register;
+using namespace uavcan::primitive::scalar;
+
+/**************************************************************************************
+ * CONSTANTS
+ **************************************************************************************/
+
+static int const NEOPIXEL_PIN    = 13;
+static int const MCP2515_CS_PIN  = 17;
+static int const MCP2515_INT_PIN = 20;
+
+static int const NEOPIXEL_NUM_PIXELS = 8; /* Popular NeoPixel ring size */
+
+static CanardNodeID const DEFAULT_AUX_CONTROLLER_NODE_ID = 20;
+
+static SPISettings  const MCP2515x_SPI_SETTING{1000000, MSBFIRST, SPI_MODE0};
+
+static CanardPortID const ID_LIGHT_MODE = 2010U;
+
+static int8_t const LIGHT_MODE_RED   = 1;
+static int8_t const LIGHT_MODE_GREEN = 2;
+static int8_t const LIGHT_MODE_BLUE  = 3;
+static int8_t const LIGHT_MODE_WHITE = 4;
+static int8_t const LIGHT_MODE_AMBER = 5;
+
+/**************************************************************************************
+ * FUNCTION DECLARATION
+ **************************************************************************************/
+
+void onReceiveBufferFull(CanardFrame const & frame);
+void onLightMode_Received(Integer8_1_0<ID_LIGHT_MODE> const & msg);
+
+/**************************************************************************************
+ * GLOBAL VARIABLES
+ **************************************************************************************/
+
+ArduinoMCP2515 mcp2515([]()
+                       {
+                         noInterrupts();
+                         SPI.beginTransaction(MCP2515x_SPI_SETTING);
+                         digitalWrite(MCP2515_CS_PIN, LOW);
+                       },
+                       []()
+                       {
+                         digitalWrite(MCP2515_CS_PIN, HIGH);
+                         SPI.endTransaction();
+                         interrupts();
+                       },
+                       [](uint8_t const d) { return SPI.transfer(d); },
+                       micros,
+                       onReceiveBufferFull,
+                       nullptr);
+
+Node::Heap<Node::DEFAULT_O1HEAP_SIZE> node_heap;
+Node node_hdl(node_heap.data(), node_heap.size(), micros, [] (CanardFrame const & frame) { return mcp2515.transmit(frame); }, DEFAULT_AUX_CONTROLLER_NODE_ID);
+
+Publisher<Heartbeat_1_0<>> heartbeat_pub = node_hdl.create_publisher<Heartbeat_1_0<>>
+  (Heartbeat_1_0<>::PORT_ID, 1*1000*1000UL /* = 1 sec in usecs. */);
+
+static Adafruit_NeoPixel neo_pixel_ctrl(NEOPIXEL_NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB);
+
+Subscription light_mode_subscription =
+  node_hdl.create_subscription<Integer8_1_0<ID_LIGHT_MODE>>(
+    ID_LIGHT_MODE,
+    CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+    [&neo_pixel_ctrl](Integer8_1_0<ID_LIGHT_MODE> const & msg)
+    {
+      if (msg.data.value == LIGHT_MODE_RED)
+        neo_pixel_ctrl.fill(neo_pixel_ctrl.Color(55, 0, 0));
+      else if (msg.data.value == LIGHT_MODE_GREEN)
+        neo_pixel_ctrl.fill(neo_pixel_ctrl.Color(0, 55, 0));
+      else if (msg.data.value == LIGHT_MODE_BLUE)
+        neo_pixel_ctrl.fill(neo_pixel_ctrl.Color(0, 0, 55));
+      else if (msg.data.value == LIGHT_MODE_WHITE)
+        neo_pixel_ctrl.fill(neo_pixel_ctrl.Color(55, 55, 55));
+      else if (msg.data.value == LIGHT_MODE_AMBER)
+        neo_pixel_ctrl.fill(neo_pixel_ctrl.Color(55, 40, 0));
+      else
+        neo_pixel_ctrl.clear();
+
+      neo_pixel_ctrl.show();
+    });
+
+/* REGISTER ***************************************************************************/
+
+static RegisterNatural8 reg_rw_cyphal_node_id         ("cyphal.node.id",          Register::Access::ReadWrite, Register::Persistent::No, DEFAULT_AUX_CONTROLLER_NODE_ID, [&node_hdl](uint8_t const & val) { node_hdl.setNodeId(val); });
+static RegisterString   reg_ro_cyphal_node_description("cyphal.node.description", Register::Access::ReadWrite, Register::Persistent::No, "L3X-Z AUX_CONTROLLER");
+static RegisterList     reg_list(node_hdl);
+
+/* NODE INFO **************************************************************************/
+
+static NodeInfo node_info
+(
+  node_hdl,
+  /* cyphal.node.Version.1.0 protocol_version */
+  1, 0,
+  /* cyphal.node.Version.1.0 hardware_version */
+  1, 0,
+  /* cyphal.node.Version.1.0 software_version */
+  0, 1,
+  /* saturated uint64 software_vcs_revision_id */
+  0,
+  /* saturated uint8[16] unique_id */
+  OpenCyphalUniqueId(),
+  /* saturated uint8[<=50] name */
+  "107-systems.l3xz-aux-ctrl"
+);
+
+Heartbeat_1_0<> hb_msg;
+
+/**************************************************************************************
+ * SETUP/LOOP
+ **************************************************************************************/
+
+void setup()
+{
+  Serial.begin(115200);
+  //while (!Serial) { }
+
+  /* Setup LED pins and initialize */
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  /* Setup SPI access */
+  SPI.begin();
+  pinMode(MCP2515_CS_PIN, OUTPUT);
+  digitalWrite(MCP2515_CS_PIN, HIGH);
+
+  /* Attach interrupt handler to register MCP2515 signaled by taking INT low */
+  pinMode(MCP2515_INT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(MCP2515_INT_PIN), []() { mcp2515.onExternalEventHandler(); }, LOW);
+
+  /* Initialize MCP2515 */
+  mcp2515.begin();
+  mcp2515.setBitRate(CanBitRate::BR_250kBPS_16MHZ);
+  mcp2515.setNormalMode();
+
+  /* Configure initial heartbeat */
+  hb_msg.data.uptime = 0;
+  hb_msg.data.health.value = uavcan_node_Health_1_0_NOMINAL;
+  hb_msg.data.mode.value = uavcan_node_Mode_1_0_INITIALIZATION;
+  hb_msg.data.vendor_specific_status_code = 0;
+
+  /* Register callbacks.
+   */
+  reg_list.add(reg_rw_cyphal_node_id);
+  reg_list.add(reg_ro_cyphal_node_description);
+
+  /* Initialize NeoPixel control. */
+  neo_pixel_ctrl.begin();
+  neo_pixel_ctrl.fill(neo_pixel_ctrl.Color(55, 55, 55));
+  neo_pixel_ctrl.show();
+}
+
+void loop()
+{
+  /* Process all pending OpenCyphal actions.
+   */
+  {
+    CriticalSection crit_sec;
+    node_hdl.spinSome();
+  }
+
+  /* Publish all the gathered data, although at various
+   * different intervals.
+   */
+  static unsigned long prev_heartbeat = 0;
+
+  unsigned long const now = millis();
+
+  /* Publish the heartbeat once/second */
+  if(now - prev_heartbeat > 1000)
+  {
+    prev_heartbeat = now;
+
+    hb_msg.data.uptime = millis() / 1000;
+    hb_msg.data.mode.value = uavcan_node_Mode_1_0_OPERATIONAL;
+    heartbeat_pub->publish(hb_msg);
+  }
+}
+
+/**************************************************************************************
+ * FUNCTION DEFINITION
+ **************************************************************************************/
+
+void onReceiveBufferFull(CanardFrame const & frame)
+{
+  node_hdl.onCanFrameReceived(frame);
+}
