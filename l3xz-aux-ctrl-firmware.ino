ae11162b33091a6fe8db44dff744002e7a3c30c2
Alexander Entinger <consulting@lxrobotics.com>
2023-05-31 20:25:56 +0200

    Fix: increasing I2C clock to 400k reduces chance of watchdog timeout during storage.

--- l3xz-aux-ctrl-firmware.ino
+++ l3xz-aux-ctrl-firmware.ino
@@ -179,6 +179,7 @@ void setup()
 
   /* LITTLEFS/EEPROM ********************************************************************/
   Wire.begin();
+  Wire.setClock(400*1000UL); /* Set fast mode. */
 
   if (!eeprom.isConnected()) {
     DBG_ERROR("Connecting to EEPROM failed.");
